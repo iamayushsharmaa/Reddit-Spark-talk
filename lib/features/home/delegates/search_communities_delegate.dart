c3078dd5ace0a9849539f5a9cabf6e19da51e3bd
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-05-15 23:51:18 +0530

    search feature

--- lib/features/home/delegates/search_communities_delegate.dart
+++ lib/features/home/delegates/search_communities_delegate.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:spark_talk_reddit/core/common/error_text.dart';
+import 'package:spark_talk_reddit/core/common/loader.dart';
+import 'package:spark_talk_reddit/features/community/controller/community_controller.dart';
 
 class SearchCommunityDelegate extends SearchDelegate {
+  final Ref ref;
+
+  SearchCommunityDelegate(this.ref);
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -23,9 +32,33 @@ class SearchCommunityDelegate extends SearchDelegate {
     return const SizedBox();
   }
 
+  void navigateToCommunityScreen(BuildContext context, String communityName) {
+    Routemaster.of(context).push('/r/${communityName}');
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return ref
+        .watch(searchCommunityProvider(query))
+        .when(
+          data:
+              (communities) => ListView.builder(
+                itemCount: communities.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final community = communities[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(community.avatar),
+                    ),
+                    title: Text("r/${community.name}"),
+                    onTap: () {
+                      navigateToCommunityScreen(context, community.name);
+                    },
+                  );
+                },
+              ),
+          error: (error, stackTrace) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
